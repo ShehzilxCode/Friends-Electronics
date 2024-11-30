0896fed9749b8de33e8f004ebb058e6e71541446
Rabi2206d <119856644+Rabi2206d@users.noreply.github.com>
2024-11-30 17:51:34 +0500

    otp update links

--- resources/views/UserUI/authlayout.blade.php
+++ resources/views/UserUI/authlayout.blade.php
@@ -104,6 +104,14 @@
         <script src="{{ URL::asset('assets/js/custom/image-preview.js')}}"></script>
         <script src="{{ URL::asset('assets/js/custom/pagination.js')}}"></script>
         <script src="{{ URL::asset('assets/js/custom/filemanager.js')}}"></script>
+
+        
+    <!-- particles js -->
+    <script src="../DashboardAsset/libs/particles.js/particles.js"></script>
+    <!-- particles app js -->
+    <script src="../DashboardAsset/js/pages/particles.app.js"></script>
+    <!-- two-step-verification js -->
+    <script src="../DashboardAsset/js/pages/two-step-verification.init.js"></script>
     
         <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.1/jquery.min.js" integrity="sha512-v2CJ7UaYy4JwqLDIrZUI/4hqeoQieOmAZNXBeQyjo21dadnwR+8ZaIJVT8EE2iyI61OV8e6M8PP2/4hpQINQ/g==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
         @stack('scripts')
